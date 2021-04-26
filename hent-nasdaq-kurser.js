7aee1b7d17f1eeb3ba64a4929509c620ae9a7b04
Bo Eschricht <boeschricht@gmail.com>
2021-04-26 23:41:49 +0200

    db still not working

--- hent-nasdaq-kurser.js
+++ hent-nasdaq-kurser.js
@@ -3,6 +3,10 @@ const log = require("log");
 const puppeteer = require("puppeteer");
 var alasql = require('alasql');
 var config = require('./config');
+// ensure user and group can read and write database created.
+const newmask = 0o000;
+const oldmask = process.umask(newmask);
+
 
 function Date_toISOStringLocal(d) {
     function z(n) {
@@ -14,10 +18,10 @@ function Time_toISOStringLocal(d) {
     function z(n) {
         return (n < 10 ? "0" : "") + n;
     }
-    return z(d.getHours() + 2) + ":" + z(d.getMinutes());
+    return z(d.getHours() + 2) + ":" + z(d.getMinutes() + ":" + z(d.getSeconds()));
 }
 
-function sleep(n) {
+function Sleep(n) {
     Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
 }
 
@@ -27,52 +31,35 @@ function sleep(n) {
     var errors = [];
     const datafile = config.datafile || "datafile-" + datetimestr;
     process.env.LOG_LEVEL = config.loglevel;
-    console.log("log level"+ process.env.LOG_LEVEL);
-    
+    console.log("log level" + process.env.LOG_LEVEL);
+
     // Load existing dataset or create a new
-    await alasql("CREATE TABLE dataset (`URL ID` string, `URL beskrivelse` string, `URL adresse` string, `Dato` date, `Tid` time, `Kurs` number, `Fejl` string)");
-    await alasql("INSERT INTO dataset VALUES ('XCSE05NYK01EA53', 'Nasdaq Nykredit 0,5%', 'http://www.nasdaqomxnordic.com/bonds/denmark/microsite?Instrument=XCSE05NYK01EA53', '20210423', '00:33', '95,4', '')");
-    await alasql("INSERT INTO dataset VALUES ('XCSE05NYK01EA53', 'Nasdaq Nykredit 0,5%', 'http://www.nasdaqomxnordic.com/bonds/denmark/microsite?Instrument=XCSE05NYK01EA53', '20210423', '00:33', '95,4', '')");
-    await alasql("INSERT INTO dataset VALUES ('XCSE05NYK01EA53', 'Nasdaq Nykredit 0,5%', 'http://www.nasdaqomxnordic.com/bonds/denmark/microsite?Instrument=XCSE05NYK01EA53', '20210423', '00:33', '95,4', '')");
-    var res3 =await alasql("SELECT * FROM dataset");
-    console.log(res3);
-    var res2 = alasql('Select * INTO json("/app/data/out.json") FROM dataset');
-    console.log(res2);
-    debugger;
-    await alasql("DELETE FROM dataset");
-    var res4 =await alasql("SELECT * FROM dataset");
-    console.log(res4);
-    
-    xxxx INSERT INTO not working!!!!
-    var res5 = await alasql('SELECT * INTO dataset FROM json("/app/data/out.json")');
-    console.log("Dataset:");
-    console.log(res5);
-
-    
-    
-    var testdata = require("/app/test-dataset");
-    var res = alasql("Select * FROM ?", [testdata]);
-    
-    
-    var res2=require("os").userInfo()
-    
-    
-    debugger;
-    var fs = require('fs');
-    fs.open('/app/data/out.json', 'w', function (err, file) {
-        if (err) {
-            debugger;
-            throw err;
-        }
-        console.log('Saved!');
+    var result = alasql.promise([
+        'CREATE FILESTORAGE DATABASE IF NOT EXISTS mydatabase("' + datafile + '")',
+        'ATTACH FILESTORAGE DATABASE mydatabase("' + datafile + '")',
+        'USE mydatabase',
+        'CREATE TABLE IF NOT EXISTS mydatabase.mydataset (`URL ID` string, `URL beskrivelse` string, `URL adresse` string, Dato date, Tid time, Kurs decimal, Fejl string)',
+    ]).then(function (result) {
         debugger;
+        console.log('Result from last query:', result.pop())
+    }).catch(function (reason) {
+        debugger;
+        console.log('Error:', reason)
     });
+
+    return;
+    alasql('SET AUTOCOMMIT ON');
+    alasql('CREATE FILESTORAGE DATABASE IF NOT EXISTS mydatabase("' + datafile + '")');
+    alasql('ATTACH FILESTORAGE DATABASE mydatabase("' + datafile + '")');
+    await alasql('USE mydatabase');
+    await alasql('CREATE TABLE IF NOT EXISTS mydataset (`URL ID` string, `URL beskrivelse` string, `URL adresse` string, Dato date, Tid time, Kurs decimal, Fejl string)');
     debugger;
 
+    // await alasql("INSERT INTO dataset VALUES ('XCSE05NYK01EA53', 'Nasdaq Nykredit 0,5%', 'http://www.nasdaqomxnordic.com/bonds/denmark/microsite?Instrument=XCSE05NYK01EA53', '20210423', '00:33', '95,4', '')");
+    // await alasql("INSERT INTO dataset VALUES ('XCSE05NYK01EA53', 'Nasdaq Nykredit 0,5%', 'http://www.nasdaqomxnordic.com/bonds/denmark/microsite?Instrument=XCSE05NYK01EA53', '20210423', '00:33', '95,4', '')");
+    // await alasql("INSERT INTO dataset VALUES ('XCSE05NYK01EA53', 'Nasdaq Nykredit 0,5%', 'http://www.nasdaqomxnordic.com/bonds/denmark/microsite?Instrument=XCSE05NYK01EA53', '20210423', '00:33', '95,4', '')");
 
-
-
-
+    return;
     const browser = await puppeteer.launch({
         args: [
             "--no-sandbox",
@@ -86,7 +73,7 @@ function sleep(n) {
 
     for (const url of config.urls) {
         try {
-            console.log("url: " + url);
+            console.log("url: " + JSON.stringify(url));
             if (url.useragent || config.defaultuseragent) {
                 console.log("Using HTTP UserAgent: " + (url.useragent || config.defaultuseragent));
                 await page.setUserAgent(url.useragent || config.defaultuseragent);
@@ -111,14 +98,15 @@ function sleep(n) {
             try {
                 console.log("waitforselector: ", url.selector);
                 // // content = await page.waitForSelector(url.selector, (el) => el.textContent);
-                content="50.5"
+                content = "50.5"
                 console.log("got it: " + content);
-                alasql("INSERT INTO dataset VALUES(?, ?, ?, ?, ?, ?)", url.URL_ID, url.URL_beskrivelse, url.URL_adresse, today.getDate(), today.getTime(), content);
+                alasql("INSERT INTO dataset VALUES(?, ?, ?, ?, ?, ?, ?)", [url.URL_ID, url.URL_beskrivelse, url.URL_adresse, Date_toISOStringLocal(today), Time_toISOStringLocal(today), content, null]);
 
                 // content = await page.$eval(contentSelector1, (el) => el.textContent);
             } catch (e) {
+                console.error("an eror occured");
                 errors.push(e + "\r\n");
-                alasql("INSERT INTO dataset VALUES(?, ?, ?, ?, ?, ?)", url.URL_ID, url.URL_beskrivelse, url.URL_adresse, today.getDate(), today.getTime(), null, e);
+                alasql("INSERT INTO dataset VALUES(?, ?, ?, ?, ?, ?, ?)", [url.URL_ID, url.URL_beskrivelse, url.URL_adresse, Date_toISOStringLocal(today), Time_toISOStringLocal(today), null, e]);
                 throw new Error(
                     "Cannot get content (content selector is probably wrong)"
                 );
@@ -126,12 +114,12 @@ function sleep(n) {
         } catch (err) {
 
         }
-        var res = alasql("Select * FROM dataset")
-        console.log("Dataset:")
-        console.log(res)
-    
     };
 
+    var res = alasql("Select * FROM dataset")
+    console.log("Dataset:")
+    console.log(res)
+    await alasql('DETACH DATABASE mydatabase');
 
     console.log("Closing Puppeteer...");
     await browser.close();
--- sh/build.sh
+++ sh/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t docker-puppeteer:latest .
+docker build $@ -t docker-puppeteer:latest .
